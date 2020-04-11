c87b96224ccd920721fd178d7a4dd57602201f91
Josue Rodriguez <smexyjosue@gmail.com>
2020-04-11 14:46:45 -0700

    Animation for Homepage/Landing Page
    * created rubber animation for software engineer text in
    animations/SoftwareEngineer.sj
    
    * installed react-animated-css as a dependency and added script link in
    index.html head

--- portfolio-v2/src/App.js
+++ portfolio-v2/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 // components
 import MobileNav from "./components/MobileNav.js";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <MobileNav />
+      <Home />
     </div>
   );
 }
--- portfolio-v2/src/components/Home.js
+++ portfolio-v2/src/components/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-export default function Home() {
+//animation
+import SoftwareEnginner from "./animations/SoftwareEnginner.js";
+
+export default function Home(props) {
+  console.log(props);
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <section id="home">
+      <div className="home-container">
+        <div className="main-header">
+          <h1>Josue Rodriguez</h1>
+          <SoftwareEnginner />
+          <h1>Portfolio</h1>
+        </div>
+        <div className="sub-header">
+          <p>
+            'When I let go of what I am,
+            <br /> I become what I might be.' - Lao Tsu
+          </p>
+        </div>
+        <div className="get-started-btn">
+          <p className="btn">Get To Know Me</p>
+        </div>
+      </div>
+    </section>
   );
 }
