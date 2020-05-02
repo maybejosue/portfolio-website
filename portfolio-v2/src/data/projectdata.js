9bcb052434064d4871c888f71494e4181446dd8e
Josue Rodriguez <jr2589213@gmail.com>
2020-05-01 20:29:10 -0700

    test

--- portfolio-v2/src/data/projectdata.js
+++ portfolio-v2/src/data/projectdata.js
@@ -17,8 +17,8 @@ export const projectData = [
   {
     id: 2,
     img:
-      "https://i.postimg.cc/85KKn8xH/Screen-Shot-2020-05-01-at-7-47-28-PM.png",
-    name: "my project 320 x 240",
+      "[Screen-Shot-2020-05-01-at-7-47-28-PM.png](https://postimg.cc/ZCGQL0K2)",
+    name: "my project 800",
     desc:
       "this is a test on the styling afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu",
     liveSite: "https://hulu.com",
@@ -32,8 +32,8 @@ export const projectData = [
   {
     id: 3,
     img:
-      "https://i.postimg.cc/7LbmGt5J/Screen-Shot-2020-05-01-at-7-47-28-PM.png",
-    name: "my project",
+      "https://i.postimg.cc/zfT2rzfD/Screen-Shot-2020-05-01-at-7-47-28-PM.png",
+    name: "my project 1024",
     desc:
       "this is a test on the styling afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu",
     liveSite: "",
@@ -47,8 +47,8 @@ export const projectData = [
   {
     id: 4,
     img:
-      "https://i.postimg.cc/N0MRm1Qv/Screen-Shot-2020-03-31-at-8-09-46-PM.png",
-    name: "my project",
+      "https://i.postimg.cc/rpTC5S8c/Screen-Shot-2020-05-01-at-7-47-28-PM.png",
+    name: "my project 2nd to last",
     desc:
       "this is a test on the styling afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu",
     liveSite: "",
