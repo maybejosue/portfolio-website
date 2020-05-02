5bef23e5525bbe329645dcd5f1625bee9fdb2d1a
Josue Rodriguez <jr2589213@gmail.com>
2020-05-01 19:49:55 -0700

    test images on mobile

--- portfolio-v2/src/data/projectdata.js
+++ portfolio-v2/src/data/projectdata.js
@@ -17,8 +17,8 @@ export const projectData = [
   {
     id: 2,
     img:
-      "https://i.postimg.cc/N0MRm1Qv/Screen-Shot-2020-03-31-at-8-09-46-PM.png",
-    name: "my project",
+      "https://i.postimg.cc/85KKn8xH/Screen-Shot-2020-05-01-at-7-47-28-PM.png",
+    name: "my project 320 x 240",
     desc:
       "this is a test on the styling afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu",
     liveSite: "https://hulu.com",
@@ -32,7 +32,7 @@ export const projectData = [
   {
     id: 3,
     img:
-      "https://i.postimg.cc/N0MRm1Qv/Screen-Shot-2020-03-31-at-8-09-46-PM.png",
+      "https://i.postimg.cc/7LbmGt5J/Screen-Shot-2020-05-01-at-7-47-28-PM.png",
     name: "my project",
     desc:
       "this is a test on the styling afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu",
