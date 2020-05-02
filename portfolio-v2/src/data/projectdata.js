26048a6b928d61c6583cc6d978ea1900d7ae8bc6
Josue Rodriguez <jr2589213@gmail.com>
2020-05-02 08:02:13 -0700

    adds buttons to project cards

--- portfolio-v2/src/data/projectdata.js
+++ portfolio-v2/src/data/projectdata.js
@@ -17,7 +17,7 @@ export const projectData = [
   {
     id: 2,
     img:
-      "[Screen-Shot-2020-05-01-at-7-47-28-PM.png](https://postimg.cc/ZCGQL0K2)",
+      "https://i.postimg.cc/N0MRm1Qv/Screen-Shot-2020-03-31-at-8-09-46-PM.png",
     name: "my project 800",
     desc:
       "this is a test on the styling afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu",
@@ -32,7 +32,7 @@ export const projectData = [
   {
     id: 3,
     img:
-      "https://i.postimg.cc/zfT2rzfD/Screen-Shot-2020-05-01-at-7-47-28-PM.png",
+      "https://i.postimg.cc/N0MRm1Qv/Screen-Shot-2020-03-31-at-8-09-46-PM.png",
     name: "my project 1024",
     desc:
       "this is a test on the styling afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu",
@@ -47,7 +47,7 @@ export const projectData = [
   {
     id: 4,
     img:
-      "https://i.postimg.cc/rpTC5S8c/Screen-Shot-2020-05-01-at-7-47-28-PM.png",
+      "https://i.postimg.cc/N0MRm1Qv/Screen-Shot-2020-03-31-at-8-09-46-PM.png",
     name: "my project 2nd to last",
     desc:
       "this is a test on the styling afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu afeuhfpau hapowhe po; sijf apiowhe baoiywefho aw aoiwuehf  iauwhe fo aiwu",
