ce235b0e7b75e3ddc30b39612151a6208bbc5cab
Josue Rodriguez <jr2589213@gmail.com>
2020-05-13 20:06:28 -0700

    adds logic to closerlook

--- portfolio-v2/src/components/CloserLook.js
+++ portfolio-v2/src/components/CloserLook.js
@@ -1,6 +1,26 @@
 import React from "react";
-
+import { useHistory, Redirect } from "react-router-dom";
+import { projectData } from "../data/projectdata.js";
 export default function CloserLook(props) {
-  console.log(`in closer look`, props);
-  return <div>Hello how are you doing my guy</div>;
+  const history = useHistory();
+
+  if (!history.location.state) {
+    const id = props.match.params.id;
+
+    let filtering = projectData.filter(
+      (projects) => parseInt(id) === projects.id
+    );
+    history.push({ state: filtering[0] });
+  }
+
+  return (
+    <div>
+      <div>
+        <div>'button'</div>
+      </div>
+      <div>
+        <h3>{history.location.state.name}</h3>
+      </div>
+    </div>
+  );
 }
