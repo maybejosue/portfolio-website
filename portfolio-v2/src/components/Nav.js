69bd403ed7e48da6c662e4224580ae5bbbad1479
Josue Rodriguez <jr2589213@gmail.com>
2020-05-16 12:08:29 -0700

    adds styling to nav desktop component

--- portfolio-v2/src/components/Nav.js
+++ portfolio-v2/src/components/Nav.js
@@ -6,23 +6,30 @@ import { Link } from "react-scroll";
 // Image
 import MyPic from "../img/professional-pic.png";
 
+//react headroom
+import Headroom from "react-headroom";
+
 export default function Nav() {
   return (
-    <div id="home" id="nav">
-      <div className="nav-img-container">
-        <img src={MyPic} className="img" />
+    <Headroom>
+      <div id="home" id="nav">
+        <div className="nav-normal-link-container">
+          <div className="nav-img-container">
+            <img src={MyPic} className="img" />
+          </div>
+          <div className="nav-links ">
+            <Link className="nav-anchor">Home</Link>
+            <Link className="nav-anchor">About</Link>
+            <Link className="nav-anchor">Skills</Link>
+            <Link className="nav-anchor">Projects</Link>
+            <Link className="nav-anchor">Resume</Link>
+            <Link className="nav-anchor">Social</Link>
+          </div>
+        </div>
+        <div className="nav-contact">
+          <Link>Contact</Link>
+        </div>
       </div>
-      <div className="nav-links ">
-        <Link className="nav-anchor">Home</Link>
-        <Link className="nav-anchor">About</Link>
-        <Link className="nav-anchor">Skills</Link>
-        <Link className="nav-anchor">Projects</Link>
-        <Link className="nav-anchor">Resume</Link>
-        <Link className="nav-anchor">Social</Link>
-      </div>
-      <div className="nav-contact">
-        <Link>Contact</Link>
-      </div>
-    </div>
+    </Headroom>
   );
 }
