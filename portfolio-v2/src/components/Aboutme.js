a956a6d6998487e405fd906efb66fdd712b10907
Josue Rodriguez <smexyjosue@gmail.com>
2020-05-22 13:42:00 -0700

    about me section display fex for desktop

--- portfolio-v2/src/components/Aboutme.js
+++ portfolio-v2/src/components/Aboutme.js
@@ -8,16 +8,18 @@ export default function Aboutme() {
     <section id="aboutme">
       <div className="about-me-container">
         <h5 className="section-heading">About Me</h5>
-        <div className="about-me-img-container">
-          <img src={fillerpic} className="about-me-img" />
-        </div>
-        <div className="about-me-paragraph-container">
-          <p>
-            filler text: sk;hapoi fhpasih foiahp wsfhpoawhspefhahpoh sk;hapoif
-            hpas ihfoiahpws fhpoaw hsp efh ahpoh sk ;ha poi fhpasih foiahpw
-            sfhpoawh spe fha hpoh sk;hap oifhpasi hfoiahpw sfhp oawh spefha hpoh
-            sk ;ha poi fh pasihf oiahpwsf hpoawh spefhahpoh
-          </p>
+        <div className="about-me-content-container">
+          <div className="about-me-img-container">
+            <img src={fillerpic} className="about-me-img" />
+          </div>
+          <div className="about-me-paragraph-container">
+            <p>
+              filler text: sk;hapoi fhpasih foiahp wsfhpoawhspefhahpoh sk;hapoif
+              hpas ihfoiahpws fhpoaw hsp efh ahpoh sk ;ha poi fhpasih foiahpw
+              sfhpoawh spe fha hpoh sk;hap oifhpasi hfoiahpw sfhp oawh spefha
+              hpoh sk ;ha poi fh pasihf oiahpwsf hpoawh spefhahpoh
+            </p>
+          </div>
         </div>
       </div>
     </section>
