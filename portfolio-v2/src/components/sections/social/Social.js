a37ab8f8411481ecefeb9a93d79f6681016ab2d3
Josue Rodriguez <jr2589213@gmail.com>
2020-10-02 22:17:40 -0700

    refactors code for Socialdata.js
    * creates social media data structure
    * maps thru new data on socialdata.js
    * removes unused css in Social.css

--- portfolio-v2/src/components/sections/social/Social.js
+++ portfolio-v2/src/components/sections/social/Social.js
@@ -6,55 +6,35 @@ import linkedin from "../../../img/linkedin-logo-x2.png";
 import github from "../../../img/github-logo-x2.png";
 import email from "../../../img/email-communication-x2.png";
 
+// social data
+import { socialdata } from "../../../data/socialdata.js";
+
 export default function Social() {
   return (
     <section id="social">
       <div className="social-container">
         <h5 className="section-heading">Social Media</h5>
 
-        <a
-          href="https://www.linkedin.com/in/josue-rodriguez-e90210"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="social-card-container"
-        >
-          <div className="social-img-container linkedin">
-            <img src={linkedin} alt="Linkedin logo" />
-          </div>
-          <div className="social-text-content-container">
-            <h6>Linkedin</h6>
-            <p>www.linkedin.com/in/josue-rodriguez-e90210</p>
-          </div>
-        </a>
-
-        <a
-          className="social-card-container"
-          href="https://github.com/JosueRodriguez-webdev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="social-img-container github">
-            <img src={github} alt="Github logo" />
-          </div>
-          <div className="social-text-content-container">
-            <h6>Github</h6>
-            <p>https://github.com/JosueRodriguez-webdev</p>
-          </div>
-        </a>
-        <a
-          href="https://mail.google.com/mail/u/0/?tab=wm&ogbl#inbox?compose=DmwnWrRsqQCRHtCkcJQnVKLFJxgKXMnpKjTvNZVkjBGfHsxQdqXNrLDGnRvCntHKbRmcttBRRScG"
-          target="_blank"
-          className="social-card-container"
-          rel="noopener noreferrer"
-        >
-          <div className="social-img-container email">
-            <img src={email} alt="Email / Gmail logo" />
-          </div>
-          <div className="social-text-content-container">
-            <h6>Email</h6>
-            <p>jr2589213@gmail.com</p>
-          </div>
-        </a>
+        {socialdata.map((data) => (
+          <a
+            key={data.id}
+            href={data.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-card-container"
+          >
+            <div
+              className="social-img-container"
+              style={{ backgroundColor: `${data.color}` }}
+            >
+              <img src={data.iconImg} alt={`${data.heading} logo`} />
+            </div>
+            <div className="social-text-content-container">
+              <h6>{data.heading}</h6>
+              <p>{data.subHeading}</p>
+            </div>
+          </a>
+        ))}
       </div>
     </section>
   );
---
+++ portfolio-v2/src/data/socialdata.js
@@ -0,0 +1,40 @@
+import linkedin from "../img/linkedin-logo-x2.png";
+import github from "../img/github-logo-x2.png";
+import email from "../img/email-communication-x2.png";
+import leetcode from "../img/leetcode.png";
+
+export const socialdata = [
+  {
+    id: 1,
+    link: "https://www.linkedin.com/in/maybejosue/",
+    color: "#0e76a8",
+    heading: "Linkedin",
+    subHeading: "https://www.linkedin.com/in/maybejosue/",
+    iconImg: linkedin,
+  },
+  {
+    id: 2,
+    link: "https://github.com/maybejosue",
+    color: "#333",
+    heading: "Github",
+    subHeading: "https://github.com/maybejosue",
+    iconImg: github,
+  },
+  {
+    id: 3,
+    link:
+      "https://mail.google.com/mail/u/0/?tab=wm&ogbl#inbox?compose=DmwnWrRsqQCRHtCkcJQnVKLFJxgKXMnpKjTvNZVkjBGfHsxQdqXNrLDGnRvCntHKbRmcttBRRScG",
+    color: "#b23121",
+    heading: "Email",
+    subHeading: "maybejosue@gmail.com",
+    iconImg: email,
+  },
+  {
+    id: 4,
+    link: "https://leetcode.com/maybejosue/",
+    color: "#f7aa00",
+    heading: "LeetCode",
+    subHeading: "https://leetcode.com/maybejosue/",
+    iconImg: leetcode,
+  },
+];
