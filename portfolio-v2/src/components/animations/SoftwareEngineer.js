636823113e6c93a06948f4bbe5df156b2997c43b
Josue Rodriguez <jr2589213@gmail.com>
2020-10-14 19:36:08 -0700

    • creates data structure for software engineer animation • refactors code by mapping through new data structure created

--- portfolio-v2/src/components/animations/SoftwareEngineer.js
+++ portfolio-v2/src/components/animations/SoftwareEngineer.js
@@ -4,6 +4,9 @@ import "./SoftwareEngineer.css";
 //animation
 import { Animated } from "react-animated-css";
 
+//animation data
+import {animatedata} from '../../data/animatedata.js'
+
 export default function SoftwareEnginner() {
   const animationType = "rubberBand";
   const duration = 5500;
@@ -12,140 +15,33 @@ export default function SoftwareEnginner() {
     <div className="software-engineer-animation-container">
       <div className="test">
         <div className="software-text">
-          <Animated
+          {animatedata.software.word.map(data => (
+            <Animated
             animationIn={animationType}
-            animationInDelay="3000"
+            animationInDelay={data.animationDelay}
             animationInDuration={`${duration}`}
             className="inf-animate"
+            key={data.id}
           >
-            <h3 className="animate">S </h3>
+            <h3 className="animate">{data.letter} </h3>
           </Animated>
-
-          <Animated
-            animationIn={animationType}
-            animationInDelay="3100"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> o</h3>
-          </Animated>
-
-          <Animated
-            animationIn={animationType}
-            animationInDelay="3200"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> f</h3>
-          </Animated>
-
-          <Animated
-            animationIn={animationType}
-            animationInDelay="3300"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> t</h3>
-          </Animated>
-
-          <Animated
-            animationIn={animationType}
-            animationInDelay="3400"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> w</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="3500"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> a</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="3600"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> r</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="3700"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> e</h3>
-          </Animated>
+          ))}
+          
         </div>
+
+
         <div className="engineer-text">
-          <Animated
+        {animatedata.engineer.word.map(data => (
+            <Animated
             animationIn={animationType}
-            animationInDelay="3800"
+            animationInDelay={data.animationDelay}
             animationInDuration={`${duration}`}
             className="inf-animate"
+            key={data.id}
           >
-            <h3 className="animate"> E</h3>
+            <h3 className="animate">{data.letter} </h3>
           </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="3900"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> n</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="4000"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> g</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="4100"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> i</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="4200"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> n</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="4300"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> e</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="4400"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> e</h3>
-          </Animated>
-          <Animated
-            animationIn={animationType}
-            animationInDelay="4500"
-            animationInDuration={`${duration}`}
-            className="inf-animate"
-          >
-            <h3 className="animate"> r</h3>
-          </Animated>
+          ))}
         </div>
       </div>
     </div>
