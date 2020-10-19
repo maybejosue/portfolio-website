4630a71db44de1123d8b43bedf4d54ead53d9c3f
Josue Rodriguez <josuerodriguez@Josues-MBP.lan1>
2020-10-19 16:43:10 -0700

    attempts to fix navbar bug on mobile devices

--- portfolio-v2/src/utils/Headroom.js
+++ portfolio-v2/src/utils/Headroom.js
@@ -3,12 +3,14 @@ var prevScrollpos = window.pageYOffset;
 export const Headroom = () => {
   var nav = document.querySelector('.headroom')
   var currentScrollPos = window.pageYOffset;
+  console.log(currentScrollPos, prevScrollpos)
   if (nav != null){
-  if (prevScrollpos > currentScrollPos) {
+    if (currentScrollPos == 0){
+      nav.classList.remove('headroom--pinned')
+    } else if(prevScrollpos > currentScrollPos) {
     
     nav.classList.add('headroom--pinned')
     nav.classList.remove('headroom--unpinned')
-
   } else {
     nav.classList.add('headroom--unpinned')
     nav.classList.remove('headroom--pinned')
