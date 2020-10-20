a9a1965b27b0df7cc14f19a943dc3f1206411421
Josue Rodriguez <maybejosue@gmail.com>
2020-10-19 17:28:43 -0700

    possible fix for mobile and web

--- portfolio-v2/src/utils/Headroom.js
+++ portfolio-v2/src/utils/Headroom.js
@@ -1,16 +1,22 @@
 // displays navbar when user scrolls up / removes navbar when user scrolls down
 var prevScrollpos = window.pageYOffset;
+var hey = document.body.scrollLeft
 export const Headroom = () => {
   var nav = document.querySelector('.headroom')
   var currentScrollPos = window.pageYOffset;
-  console.log(currentScrollPos, prevScrollpos)
+  console.log(currentScrollPos, prevScrollpos, hey)
   if (nav != null){
-
-    if (prevScrollpos > currentScrollPos) {
+     console.log(currentScrollPos)
+     if (0 < currentScrollPos & currentScrollPos < 150){
       nav.style.top = "0";
-    } else {
-      nav.style.top = "-150px";
-    }
+    } else if (prevScrollpos > currentScrollPos) {
+      nav.style.top = "0";
+
+  } else {
+    nav.style.top = "-150px";
+
+    
+  }
 }
 
 
