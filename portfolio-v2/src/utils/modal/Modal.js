023831a4703785428eb18f1237c2eac2710aa9d0
Josue Rodriguez <maybejosue@gmail.com>
2020-10-29 19:04:04 -0700

    • Replaces exit svg function with a bigger / more appropriate exit icon • Continues development of modal with more CSS styling and HTML attributes

--- portfolio-v2/src/utils/modal/Modal.js
+++ portfolio-v2/src/utils/modal/Modal.js
@@ -1,32 +1,38 @@
 import React from "react";
 import "./Modal.css";
-import { Exit } from "../index.js";
+import { Exit, LiveView, SourceCode } from "../index.js";
 
 export const Modal = (props) => {
   console.log(props);
   return (
-    <div className="modal">
-      <div className="modal-exit-btn" onClick={props.toggle}>
-        <Exit />
-      </div>
-      <div>
-        <div className="modal-img-container">
-          <img src={props.project.img} className="modal-img" alt="project" />
+    <div className="modal-backdrop">
+      <div className="modal">
+        <div className="modal-exit-btn" onClick={props.toggle}>
+          <Exit />
         </div>
+        <div>
+          <div className="modal-img-container">
+            <img src={props.project.img} className="modal-img" alt="project" />
+          </div>
 
-        <h4>{props.project.name}</h4>
-        <p>
-          <span>App Desc:</span>
-          {props.project.appDesc}
-        </p>
-        <br />
+          <h4 className="modal-title">{props.project.name}</h4>
+          <p className="modal-techstack">{props.project.techStack}</p>
 
-        <p>
-          <span>Tech Stack:</span>
-          {props.project.techStack}
-        </p>
+          <h5>Description:</h5>
+
+          <p className="modal-app-desc">{props.project.appDesc}</p>
+          <br />
+          <div className="modal-btn-container">
+            <div className="modal-code-info-btn">
+              <SourceCode />
+            </div>
+            <div className="modal-code-info-btn">
+              <LiveView />
+            </div>
+          </div>
+        </div>
+        <div>{/* responsibilites in here */}</div>
       </div>
-      <div>{/* responsibilites in here */}</div>
     </div>
   );
 };
