8e0a9357aff3f1cb6440cb2e760ebaa4d7c09ac3
Josue Rodriguez <maybejosue@gmail.com>
2020-10-30 20:53:56 -0700

    • Removes any remaining files and code relates to closerlook

--- portfolio-v2/src/App.js
+++ portfolio-v2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
@@ -9,47 +9,49 @@ import Home from "./components/sections/home/Home.js";
 import Aboutme from "./components/sections/aboutme/Aboutme.js";
 import ProjectList from "./components/sections/projects/ProjectList.js";
 import Social from "./components/sections/social/Social.js";
-import CloserLook from "./components/sections/closerlook/CloserLook.js";
 import Resume from "./components/sections/resume/Resume.js";
 
-import {Headroom} from './utils/index'
+import { Headroom } from "./utils/index";
 
 function App() {
   const [closeNav, setCloseNav] = useState(true);
-  const [mobileNav, setMobileNav] = useState(window.innerWidth)
+  const [mobileNav, setMobileNav] = useState(window.innerWidth);
 
-   window.addEventListener('resize', () => {
-  var newWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-   
-  setMobileNav(newWidth)
-})
+  window.addEventListener("resize", () => {
+    var newWidth =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
 
+    setMobileNav(newWidth);
+  });
 
-  window.onscroll = Headroom
-
-
+  window.onscroll = Headroom;
 
   return (
-    <section id='home'>
-    <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <div>
-            {/* Keeps tabs in window width in order to switch between mobile and desktop navbar */}
-          {mobileNav > 850 ? (<Nav />):(<MobileNav closeNav={closeNav} setCloseNav={setCloseNav} />)}
+    <section id="home">
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <div>
+              {/* Keeps tabs in window width in order to switch between mobile and desktop navbar */}
+              {mobileNav > 850 ? (
+                <Nav />
+              ) : (
+                <MobileNav closeNav={closeNav} setCloseNav={setCloseNav} />
+              )}
 
-            <div onClick={() => setCloseNav(true)}>
-              <Home />
-              <Aboutme />
-              <ProjectList />
-              <Social />
+              <div onClick={() => setCloseNav(true)}>
+                <Home />
+                <Aboutme />
+                <ProjectList />
+                <Social />
+              </div>
             </div>
-          </div>
-        </Route>
-        <Route path="/cl/:id" component={CloserLook} />
-        <Route path="/resume" component={Resume} />
-      </Switch>
-    </div>
+          </Route>
+          <Route path="/resume" component={Resume} />
+        </Switch>
+      </div>
     </section>
   );
 }
