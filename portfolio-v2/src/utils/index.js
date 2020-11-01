8d0c609f4aa0c512fd24122770c982107f6680a4
Josue Rodriguez <maybejosue@gmail.com>
2020-10-31 21:28:12 -0700

    • Removes GoBack icon that was no longer being used

--- portfolio-v2/src/utils/index.js
+++ portfolio-v2/src/utils/index.js
@@ -1,9 +1,8 @@
-import { GoBack } from "./icons/GoBack.js";
 import { LiveView } from "./icons/LiveView.js";
 import { SourceCode } from "./icons/SourceCode.js";
 import { Expand } from "./icons/Expand.js";
 import { Carrot } from "./icons/Carrot.js";
-import {Headroom} from '../utils/Headroom.js'
-import {Exit} from './icons/Exit.js'
+import { Headroom } from "../utils/Headroom.js";
+import { Exit } from "./icons/Exit.js";
 
-export { GoBack, LiveView, SourceCode, Expand, Carrot, Headroom, Exit};
+export { LiveView, SourceCode, Expand, Carrot, Headroom, Exit };
